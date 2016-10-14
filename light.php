875814e5fa074794d61f6800ecdce9733ac928d4
Brijesh Srivastava <Brijesh.Srivastava@sqs.com>
2016-10-14 16:54:23 +0530

    Updated the code for Validation. Display graph according to Dropdown and Date option.

--- light.php
+++ light.php
@@ -43,20 +43,67 @@ echo "</select>";
 $Q_LID = "Please Select the parameter and generate Report.";
 $totalTimeON = 0;
 
+$LID_ERR = $DateOption_Err = $FromDate_Err = $ToDate_Err = $TimePeriod_Err = "";
 
 if(isset($_POST['submit']))
 {
-$Q_LID = $_POST['LID'];  // Storing Selected Value In Variable
-$Q_FromDate = $_POST['FromDate'];  // Storing Selected Value In Variable
-$Q_ToDate = $_POST['ToDate'];  // Storing Selected Value In Variable
-$Q_TimePeriod = $_POST['TimePeriod'];  // Storing Selected Value In Variable
-$Q_DateOption = $_POST['DateOption'];
-//$Q_ToDate = $_POST['ToDate'];  // Storing Selected Value In Variable
 //echo $Q_LID;
 //echo $Q_TimePeriod;
 //echo $Q_FromDate;
 //echo $Q_ToDate;
 //echo $Q_DateOption;
+
+If (empty($_POST['LID']))
+{
+	$LID_ERR = "Please select Light from dropdown options.";
+}
+else
+{
+	$Q_LID = $_POST['LID'];   // Storing Selected Value In Variable
+}
+
+
+If (!empty($_POST['DateOption']))
+{
+	$Q_DateOption = $_POST['DateOption']; // Storing Selected Value In Variable
+
+	if ($Q_DateOption == 'Date')
+	{
+		If (!empty($_POST['FromDate']))
+			{
+				$Q_FromDate = $_POST['FromDate'];  // Storing Selected Value In Variable
+			}
+			else
+			{
+				$FromDate_Err = "Please add From Date.";	
+			}
+
+		If (!empty($_POST['ToDate']))
+			{
+				$Q_ToDate = $_POST['ToDate'];  // Storing Selected Value In Variable
+			}
+			else
+			{
+				$ToDate_Err = "Please add TO Date.";	
+			}
+	}
+	
+	if ($Q_DateOption == 'DD')
+	{
+		If (!empty($_POST['TimePeriod']))
+			{
+				$Q_TimePeriod = $_POST['TimePeriod'];  // Storing Selected Value In Variable
+			}
+			else
+			{
+				$TimePeriod_Err = "Please select Time Period.";	
+			}
+	}
+}
+else
+{
+	$DateOption_Err = "Please select Date Option Radio button.";
+}
 
 $g_sqlq ="select EMP_REL_MAP.ReaderHWID,RellayLightMap.LightNumber,loginDetails.UEmpID,loginDetails.ReaderHardWareID, loginDetails.DateTime,loginDetails.type from loginDetails, EMP_REL_MAP,RellayLightMap Where loginDetails.UEmpID = EMP_REL_MAP.UEMPID AND RellayLightMap.RellayID = EMP_REL_MAP.RellayID AND RellayLightMap.LightNumber = '$Q_LID'";
 
@@ -164,7 +211,6 @@ If ( $R_count != 1)
 				//Print_r($EMPHWIDIN);
 				//Print_r($EMPHWIDOUT);
 
-
 				$TimePeriodIN = (in_array($a[$i]['ReaderHardWareID'], array($EMPHWIDIN,108,114,102,116,106,110,104,112)) && date_format($a[$i]['DateTime'],"Y-m-d") >= $Q_FromDate && date_format($a[$i]['DateTime'],"Y-m-d") <= $Q_ToDate);
 				$TimePeriodOUT = (in_array($a[$i]['ReaderHardWareID'], array($EMPHWIDOUT,109,115,103,117,107,111,105,113)) && date_format($a[$i]['DateTime'],"Y-m-d") >= $Q_FromDate && date_format($a[$i]['DateTime'],"Y-m-d") <= $Q_ToDate);
 				$datetime1 = new DateTime($Q_FromDate);
@@ -184,8 +230,8 @@ If ( $R_count != 1)
 					//Print_r($Time1);
 					//$Temp = $a[$i]['ReaderHardWareID'];
 					//Print_r($Temp);
-
 				}
+				
 			If($a[$i]['type']=='logout' && $TimePeriodOUT)
 				{
 					If(empty($Time1))
@@ -266,47 +312,77 @@ If ( $R_count != 1)
 	 
 	  
 </script>
+<style>
+.error {color: #FF0000;}
+</style>
 </head>
-<form action="#" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 <body>
 <table width="100%" height="100%" border="1">
   <tr>
-    <td height="46" colspan="2"><div align="center">Electricity Consumption </div></td>
+    <td height="46" colspan="3" Align="center"><H2>Electricity Consumption</H2>
+	<p><span class="error">* required field.</span></p>
+	</td>
   </tr>
     <tr>
-	<td><?php GetOptions($result); ?>
-		<input type="radio" name="DateOption" value="DD">Dropdown
-		<input type="radio" name="DateOption" value="Date">Date
-		<?php echo "<select name='TimePeriod'>
+	<td colspan="3">Select Light: <?php GetOptions($result); ?> 
+	<span class="error">* <?php echo $LID_ERR;?></span>
+	</td>
+  </tr>
+      <tr>
+	<td><input type="radio" name="DateOption" value="DD">Dropdown <span class="error">* <?php echo $DateOption_Err;?></span>	
+		<br>Select Time Period: <?php echo "<select name='TimePeriod'>
+		    <option value=''>Select</option>
 			<option value='Daily'>Daily</option>
 			<option value='Weekly'>Weekly</option>
 			<option value='Monthly'>Monthly</option>
 			<option value='Yearly'>Yearly</option>
 			</select>"; ?>
-		<input type="date" id="FromDate" name="FromDate">
-		<input type="date" id="ToDate" name="ToDate">
+		    <span class="error">* <?php echo $TimePeriod_Err;?></span>	</br>
+		
+	</td>
+		<td>
+		<input type="radio" name="DateOption" value="Date">Date <span class="error">* <?php echo $DateOption_Err;?></span>	
+		<br> Select Date Range: <input type="date" id="FromDate" name="FromDate"> 
+		<span class="error">* <?php echo $FromDate_Err;?></span>	
+		<input type="date" id="ToDate" name="ToDate"> 
+		<span class="error">* <?php echo $ToDate_Err;?></span></br>
+	</td>
+	<td>
 		<input type="submit" name="submit" value="Generate Report" />
 	</td>
   </tr>
   <tr>
     <td width="50%" height="98%"><div id="piechart" style="width:100%; height:100%;"></div>
 </td>
-	<td width="50%">
+	<td width="50%" colspan="2">
 	  <table width="100%" height="10" border="1" >
 	    <tr  height="2">
-		  <td> LightNumber</td>
-		  <td> Consumtion (hours)</td>
+		  <td width="30%"> Light Number</td>
+		  <td width="70%"> <?=$Q_LID?></td>
 		</tr>
 		<tr  height="2">
-		  <td><?=$Q_LID?></td>
-		  <td><?=$totalTimeON?></td>
+		  <td>Consumtion (hours)</td>
+		  <td><?=round ($totalTimeON,2)?></td>
+		</tr>
+		<tr  height="2">
+		  <td> Wattage Consumed</td>
+		  <td><?=round($totalTimeON*5,2)?> Watt</td>
+		</tr>
+		<tr  height="2">
+		  <td>Remaining Life Span</td>
+		  <td><?=round(8000-$totalTimeON,2)?></td>
+		</tr>
+		<tr  height="2">
+		  <td>Quality of Bulb</td>
+		  <td>NA</td>
 		</tr>
 	  </table>
 	</td>
 	
   </tr>
   <tr>
-    <td colspan="2" height="2%">&nbsp;</td>
+    <td colspan="3" height="2%">&nbsp;</td>
   </tr>
 </table>
 </body>
