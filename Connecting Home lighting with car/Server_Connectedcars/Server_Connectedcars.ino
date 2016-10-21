daa1ac2d6df699b08bb4f17c946dac5889ed6d31
Mayuresh Joshi <Mayuresh.Joshi@sqs.com>
2016-10-21 16:35:48 +0530

    Node MCU as Server and Pi as Client
    Here Pi will send a request to node mcu as the request is received then relay are triggered.

--- Connecting Home lighting with car/Server_Connectedcars/Server_Connectedcars.ino
+++ Connecting Home lighting with car/Server_Connectedcars/Server_Connectedcars.ino
@@ -1,23 +1,14 @@
 #include <ESP8266WiFi.h>
-const char* ssid = "SQS";
-const char* password = "d0n773llN0On3";
+const char* ssid = "*";
+const char* password = "*";
 
 
 //SQS
-IPAddress ip(192, 168, 163, 150);   
-IPAddress gateway(192, 168, 160, 1); 
-IPAddress subnet(255,255,255,0); 
-IPAddress dns1(192, 168, 192,4); 
-IPAddress dns2(192, 168, 70,58);
-
-
-//IOT 
-//IPAddress ip(192, 168, 163, 150);   
-//IPAddress gateway(192, 168, 0, 1); 
-//IPAddress subnet(255,255,225,0); 
-//IPAddress dns1(192, 168, 0, 1); 
-//IPAddress dns2(192, 168, 0, 1);
-
+IPAddress ip(*, *, *, *);   
+IPAddress gateway(*, *, *, *); 
+IPAddress subnet(*,*,*,*); 
+IPAddress dns1(*, *, *,*); 
+IPAddress dns2(*, *, *,*);
 
 #define D1  16
 #define D2  5
